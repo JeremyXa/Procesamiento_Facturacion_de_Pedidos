7432d8cde3ad8a34ef58ae2e76a8a93157ecedfa
JeremyXa <jeremyhv2000@gmail.com>
2025-11-03 18:15:10 -0500

    Subida del archivo e inicio de la tarea

---
+++ src/procesamiento_facturación/de/pedidos/Procesamiento_FacturaciónDePedidos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procesamiento_facturación.de.pedidos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Procesamiento_FacturaciónDePedidos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
